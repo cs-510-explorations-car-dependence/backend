b38262da3d90fe00010d5b04c214b48c03312f77
jake-bickle <bickle.jake@gmail.com>
2021-07-27 16:46:08 -0700

    Removed the _2 in OVERPASS_API_KEY

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -7,5 +7,5 @@ def get_config():
     return {
         "SECRET_KEY": os.environ.get('SECRET_KEY'),
         "HERE_TRAFFIC_API_KEY": os.environ.get("HERE_TRAFFIC_API_KEY") if not dev_mode else os.environ.get("DEV_HERE_TRAFFIC_API_KEY"),
-        "OVERPASS_API_KEY_2": os.environ.get("OVERPASS_API_KEY") if not dev_mode else os.environ.get("DEV_OVERPASS_API_KEY"),
+        "OVERPASS_API_KEY": os.environ.get("OVERPASS_API_KEY") if not dev_mode else os.environ.get("DEV_OVERPASS_API_KEY"),
     }
