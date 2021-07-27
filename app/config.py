6fd77d86f16954f4b54e6fc52fee87ff81cbcb1c
jake-bickle <bickle.jake@gmail.com>
2021-07-27 15:28:33 -0700

    Update .env variable names. Add some new variables.

--- app/config.py
+++ app/config.py
@@ -4,14 +4,14 @@ ProductionConfig = {
     "ENV": "production",
     "DEBUG": False,
     "SECRET_KEY": os.environ.get('SECRET_KEY'),
-    "SOME_API_KEY_1": os.environ.get("SOME_API_KEY_1"),
-    "SOME_API_KEY_2": os.environ.get("SOME_API_KEY_2"),
+    "HERE_TRAFFIC_API_KEY": os.environ.get("HERE_TRAFFIC_API_KEY"),
+    "OVERPASS_API_KEY_2": os.environ.get("OVERPASS_API_KEY"),
 }
 
 DevelopmentConfig = {
     "ENV": "development",
     "DEBUG": True,
     "SECRET_KEY": os.environ.get('SECRET_KEY'),
-    "SOME_API_KEY_1": os.environ.get("DEV_SOME_API_KEY_1"),
-    "SOME_API_KEY_2": os.environ.get("DEV_SOME_API_KEY_2"),
+    "HERE_TRAFFIC_API_KEY": os.environ.get("DEV_HERE_TRAFFIC_API_KEY"),
+    "OVERPASS_API_KEY_2": os.environ.get("DEV_OVERPASS_API_KEY"),
 }
