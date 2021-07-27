7313d67d53d81d6528750f194f9e1204c1489ff6
jake-bickle <bickle.jake@gmail.com>
2021-07-27 16:31:13 -0700

    Fix development and production on config

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -3,10 +3,8 @@ from dotenv import load_dotenv
 
 def get_config():
     load_dotenv()
-    dev_mode = os.environ.get("DEBUG").lower() == "yes"
+    dev_mode = os.environ.get("FLASK_ENV").lower() == "development"
     return {
-        "ENV": "development" if dev_mode else "production",
-        "DEBUG": dev_mode,
         "SECRET_KEY": os.environ.get('SECRET_KEY'),
         "HERE_TRAFFIC_API_KEY": os.environ.get("HERE_TRAFFIC_API_KEY") if not dev_mode else os.environ.get("DEV_HERE_TRAFFIC_API_KEY"),
         "OVERPASS_API_KEY_2": os.environ.get("OVERPASS_API_KEY") if not dev_mode else os.environ.get("DEV_OVERPASS_API_KEY"),
