d7db21e8b77288cc99ba94972f3966bb12c2c160
jake-bickle <bickle.jake@gmail.com>
2021-07-29 09:39:13 -0700

    Add additional highway filters

--- flaskapp/resources/overpass.py
+++ flaskapp/resources/overpass.py
@@ -1,11 +1,28 @@
 import requests
 from flaskapp.resources.utils import raise_if_invalid_coordinates
 
+# https://wiki.openstreetmap.org/wiki/Key:highway
 WAY_FILTERS = (
                 """["highway"]"""
-                """["v"!="footway"]"""
-                """["v"!="path"]"""
-                """["v"!="pedestrian"]"""
+                """["highway"!="motorway_link"]"""
+                """["highway"!="trunk_link"]"""
+                """["highway"!="primary_link"]"""
+                """["highway"!="secondary_link"]"""
+                """["highway"!="tertiary_link"]"""
+                """["highway"!="service"]"""
+                """["highway"!="pedestrian"]"""
+                """["highway"!="track"]"""
+                """["highway"!="bus_guideway"]"""
+                """["highway"!="escape"]"""
+                """["highway"!="raceway"]"""
+                """["highway"!="footway"]"""
+                """["highway"!="bridleway"]"""
+                """["highway"!="steps"]"""
+                """["highway"!="corridor"]"""
+                """["highway"!="path"]"""
+                """["highway"!="cycleway"]"""
+                """["highway"!="proposed"]"""
+                """["highway"!="construction"]"""
               )
 
 def get_road_data(upperleftbb, lowerrightbb):
@@ -13,6 +30,7 @@ def get_road_data(upperleftbb, lowerrightbb):
     raise_if_invalid_coordinates(lowerrightbb)
     url = build_url(upperleftbb, lowerrightbb)
     response = requests.get(url)
+    # TODO Convert to something usable
 
 def build_url(upperleftbb, lowerrightbb):
     return (
