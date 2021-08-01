48c5ebb1a2589ae1711c11e6b6d5e475c74cf7b2
jake-bickle <bickle.jake@gmail.com>
2021-08-01 11:20:28 -0700

    Remove Overpass API related information. get_road_data computes HERE information only

--- flaskapp/road.py
+++ flaskapp/road.py
@@ -16,83 +16,86 @@ class RoadType(Enum):
     UNKNOWN = 9        # Yes, "Road" is equivalent to "Unknown"
 
 class Road:
-    ASSUMED_LANECOUNT = 1   # The amount of lanes on a road to assume if information is not available
+
     """ Contains all significant data we care about in a road that can be aquired from resources. """
     def __init__(self):
         self.name = ""
-        self.type = RoadType.UNKNOWN
-        self.shape = []   # Array of Segments
-        self.lane_count_positive = None  # Lanes going in one direction
-        self.lane_count_negative = None  # Lanes going in the opposite direction
+        self.segments = []
 
-    def update_via_overpass_schema(self, way):
-        """
-        Parse a way type (from the Overpass JSON schema) and updates self based on what it finds.
-        """
-        tags = way["tags"]
-        self.name = self.normalize_road_name(tags["name"]) if "name" in tags else ""
-        self.type = RoadType(tags["highway"].upper()) if "highway" in tags else RoadType.UNKNOWN
-        if "oneway" in tags and tags["oneway"] == "yes":
-            self.lane_count_positive = tags["lanes:forward"] if "lanes:forward" in tags else 1
-            self.lane_count_negative = 0
-        else:
-            self.lane_count_positive = tags["lanes:forward"] if "lanes:forward" in tags else Road.ASSUMED_LANECOUNT
-            self.lane_count_negative = tags["lanes:forward"] if "lanes:forward" in tags else Road.ASSUMED_LANECOUNT
-
-    def update_via_here_flow_item(self, here_fi):
+    # Each flow item is a segment of a Road.
+    def add_flow_item(self, here_fi):
         """
         Parses a flow item and updates the Road's segments with their current traffic flow.
         """
-        # TODO Blocked. Awaiting to hear back from HERE Development Support. https://stackoverflow.com/questions/68582681/how-do-i-couple-segmented-cf-traffic-data-ss-with-shape-data-shp
-        pass
-
-    @staticmethod
-    def normalize_road_name(name):
-        """ Normalizes string: name by updating/removing characters deemed irrelevent for road name identification. """
-        return name.strip().lower()
+        self.name = here_fi["TMC"]["DE"]
+        segment = Segment()
+        segment.update_via_here_flow_item(here_fi)
+        self.segments.append(segment)
 
 class Segment:
     def __init__(self):
-        self.first_coord_pair = None  # (latitude, longitude) tuple
-        self.second_coord_pair = None
-        self.jam_factor = None
+        self.type = RoadType.UNKNOWN
+        self.shape = []  # Array of (latitude, longitude tuple)
+        self.jam_factor = None   # Float ranging from 0 to 10, as defined by HERE
         self.segment_closed = False
-        self.length = 0
+        self.length = 0  # Kilometers
 
-def get_road_data(here_dict_response, overpass_dict_response):
-    possible_roads = []
-    returned_roads = []
-    for obj in overpass_dict_response:
-        # TODO This doesn't work. Overpass breaks roads up into multiple ways. Should have expected that.
-        if obj["type"] == "way":
-            new_road = Road()
-            new_road.update_overpass_schema(obj)
-            if new_road.name:
-                possible_roads.append(new_road)
-    for roadway in here_dict_response["RWS"]["RW"]:
-        flow_item = roadway["FIS"][0]["FI"][0] # TODO In what scenario is there multiple FL (Flow items)? Shouldn't a roadway have one flow item with one name?
-        name = flow_item["TMC"]["DE"].lower()
-        road_to_update = _guess_road(name, possible_roads.values())
-        if road_to_update is None:
-            continue  # Road data wasn't in HERE response.
-        road_to_update.update_via_here_flow_item(flow_item)
-        returned_roads.append(road_to_update)
-    return returned_roads
+    def update_via_here_flow_item(self, here_fi):
+        function_class_count = {   # Each SHP has a function class, indicating road type. Each segment's type is 
+            1: 0,                  # defined as the most common function class that occured in HERE's SHP data.
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0
+        }
+        for SHP in here_fi["SHP"]:
+            function_class = SHP["FC"]
+            function_class_count[function_class] += 1
+            for value in SHP["value"]:  # I've never seen a "value" array hold more than one string, but playing it safe
+                for coordinate_string in value.strip().split(" "):
+                    coordinates = self._get_lat_long_from_string(coordinate_string)
+                    self.shape.append(coordinates)
+        function_class = max(function_class_count, key=function_class_count.get)
+        self.type = self.convert_function_class_to_roadtype(self.function_class)
 
-MINIMUM_JARO_SIMILARITY = 0.9
-def _guess_road(name, roads):
-    """
-    Given a string: name and a list of Road, returns the Road in the list whose name is closest to the name argument.
-    If no plausible Road exists, returns None.
-    """
-    comparison_name = Road.normalize_road_name(name)
-    most_likely_road = None
-    highest_similarity = 0
-    for road in roads:
-        if highest_similarity == 1:
-            return most_likely_road
-        other_name = road.name
-        similarity = Levenshtein.jaro(comparison_name, other_name)
-        if similarity > highest_similarity and similarity > MINIMUM_JARO_SIMILARITY:
-            most_likely_road = road
-    return most_likely_road
+        # HERE sometimes splits flow data into even finer segments within an "SSS" object inside the "CF" object.
+        # We disregard these finer segments, since there's no way to correlate said data to the shape data found above.
+        flow_info = here_fi["CF"][0]  # I've never seen more than a single "CF" object. We will assume the first.
+        self.jam_factor = flow_info["JF"]
+        self.segment_closed = self.jam_factor == 10
+        self.length = here_fi["TMC"]["LE"]
+
+    @staticmethod
+    def _get_lat_long_from_string(string):
+        lat_str, long_str = string.split(",")
+        return (float(lat_str), float(long_str))
+
+    @staticmethod
+    def convert_function_class_to_roadtype(function_class):
+        # Values loosely defined at https://developer.here.com/documentation/routing/dev_guide/topics/resource-type-functional-class.html
+        if function_class == 1:     # Equivalent to some parts of CA-126 https://www.google.com/maps/place/34%C2%B027'11.1%22N+118%C2%B036'56.3%22W
+            return RoadType.MOTORWAY
+        elif function_class == 2:   # Equivalent to some parts of I-5  https://www.google.com/maps/place/45%C2%B035'11.0%22N+122%C2%B040'48.2%22W 
+            return RoadType.TRUNK
+        elif function_class == 3:   # Equivalent to NE Columbia Blvd, Portland OR
+            return RoadType.PRIMARY
+        elif function_class == 4:   # Equivalent to N Chautauqua Blvd, Portland OR
+            return RoadType.UNCLASSIFIED
+        else:  # function_class == 5. Equivalent to N Willamette Blvd, Portland OR
+            return RoadType.RESIDENTIAL
+        
+
+def get_road_data(here_dict_response):
+    roads = {}
+    for RWS_obj in here_dict_response["RWS"]:  # I've never seen RWS hold more than one object, but playing it safe
+        for roadway in RWS_obj["RW"]:
+            for FIS_obj in roadway["FIS"]:   # I have never seen FIS hold more than one object, but playing it safe
+                for flow_item in FIS_obj["FI"]:
+                    name = flow_item["TMC"]["DE"]
+                    if name in roads:
+                        road = roads[name]
+                    else:
+                        road = Road()
+                        roads[name] = road
+                    road.update_via_here_flow_item(flow_item)
+    return roads.values()
