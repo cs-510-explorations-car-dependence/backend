49d195ea71e2fe894c41587a1b3d8973e53e385f
jake-bickle <bickle.jake@gmail.com>
2021-08-01 14:04:56 -0700

    Implement coordinates_are_valid

--- flaskapp/resources/utils.py
+++ flaskapp/resources/utils.py
@@ -8,3 +8,10 @@ def raise_if_invalid_coordinates(lat_long_pair):
 		raise InvalidCoordinates(f"Latitude ({lat}) must be between -90 and 90 degrees")
 	if long < -180 or long > 180:
 		raise InvalidCoordinates(f"Longitude ({long}) must be between -90 and 90 degrees")
+
+def coordinates_are_valid(lat_long_pair):
+	try:
+		raise_if_invalid_coordinates(lat_long_pair)
+		return True
+	except InvalidCoordinates:
+		return False
