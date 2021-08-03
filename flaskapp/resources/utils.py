a9fe6e9f5ca83dd4eeb4bb4c84b7ac977b6c2881
jake-bickle <bickle.jake@gmail.com>
2021-08-03 08:36:39 -0700

    Fix tab spacing

--- flaskapp/resources/utils.py
+++ flaskapp/resources/utils.py
@@ -1,17 +1,17 @@
 class InvalidCoordinates(BaseException):
-	pass
+    pass
 
 def raise_if_invalid_coordinates(lat_long_pair):
-	lat = lat_long_pair[0]
-	long = lat_long_pair[1]
-	if lat < -90 or lat > 90:
-		raise InvalidCoordinates(f"Latitude ({lat}) must be between -90 and 90 degrees")
-	if long < -180 or long > 180:
-		raise InvalidCoordinates(f"Longitude ({long}) must be between -90 and 90 degrees")
+    lat = lat_long_pair[0]
+    long = lat_long_pair[1]
+    if lat < -90 or lat > 90:
+        raise InvalidCoordinates(f"Latitude ({lat}) must be between -90 and 90 degrees")
+    if long < -180 or long > 180:
+        raise InvalidCoordinates(f"Longitude ({long}) must be between -90 and 90 degrees")
 
 def coordinates_are_valid(lat_long_pair):
-	try:
-		raise_if_invalid_coordinates(lat_long_pair)
-		return True
-	except InvalidCoordinates:
-		return False
+    try:
+        raise_if_invalid_coordinates(lat_long_pair)
+        return True
+    except InvalidCoordinates:
+        return False
